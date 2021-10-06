2ae18389f2cdc4ad3713f5a3d945373272d3b6db
reactDev777 <reactdevs777>
2021-10-06 18:41:02 +0500

    Making Responsive Home Screen

--- src/screens/Home/HomeScreen.jsx
+++ src/screens/Home/HomeScreen.jsx
@@ -11,47 +11,80 @@ const HomeScreen = () => {
   return (
     <>
       <div className="row mt-5">
-        <div className="col-lg-5 col-sm-10  col-md-10">
-          <img src={slide1} alt="" className="imgslideLeft img-fluid " />
+        <div className="col-lg-4 col-sm-12  col-md-6">
+          <div className="card">
+            <img src={slide1} alt="" className="imgslideLeft img-fluid " />
+          </div>
         </div>
-        <div className="col-lg-7 col-sm-12 col-md-12">
+        <div className="col-lg-8 col-sm-12 col-md-6">
           <div className="row">
-            <div className="col-lg-4   offset-1 offset-lg-0 offset-md-0 col-md-6  col-sm-12 padClass rightHeroVid">
-              <p className="text-white">PHYGITAL TOY</p>
-              <video
-                playsInline
-                autoPlay
-                loop
-                src={video2}
-                className="rightVideo"
-              ></video>
+            <div className="col-lg-4  col-md-6  col-sm-12 padClass rightHeroVid">
+              <div className="card">
+                <p className="text-white">PHYGITAL TOY</p>
+                <video
+                  playsInline
+                  autoPlay
+                  loop
+                  src={video2}
+                  className="rightVideo"
+                ></video>
+              </div>
             </div>
-            <div className="col-lg-4 offset-1 offset-lg-0 offset-md-0   col-md-6 col-sm-12  padClass rightHeroVid">
-              <p className="text-white">PHYGITAL TOY</p>
-              <video
-                playsInline
-                autoPlay
-                loop
-                src={video3}
-                className="rightVideo"
-              ></video>
+            <div className="col-lg-4    col-md-6 col-sm-12  padClass rightHeroVid">
+              <div className="card">
+                <p className="text-white">PHYGITAL TOY</p>
+                <video
+                  playsInline
+                  autoPlay
+                  loop
+                  src={video3}
+                  className="rightVideo"
+                ></video>
+              </div>
             </div>
-
-            <div className="col-lg-4 offset-1 offset-lg-0 offset-md-0   col-md-6 col-sm-12  padClass rightHeroVid">
-              <p className="text-white">PHYGITAL TOY</p>
-              <video autoPlay loop src={video3} className="rightVideo"></video>
+            <div className="col-lg-4     col-md-6 col-sm-12  padClass rightHeroVid">
+              <div className="card">
+                <p className="text-white">PHYGITAL TOY</p>
+                <video
+                  autoPlay
+                  loop
+                  src={video3}
+                  className="rightVideo"
+                ></video>
+              </div>
             </div>
-            <div className="col-lg-4 offset-1 offset-lg-0 offset-md-0   col-md-6 col-sm-12  padClass rightHeroVid">
-              <p className="text-white">PHYGITAL TOY</p>
-              <video autoPlay loop src={video4} className="rightVideo"></video>
+            <div className="col-lg-4     col-md-6 col-sm-12  padClass rightHeroVid">
+              <div className="card">
+                <p className="text-white">PHYGITAL TOY</p>
+                <video
+                  autoPlay
+                  loop
+                  src={video4}
+                  className="rightVideo"
+                ></video>
+              </div>
             </div>
-            <div className="col-lg-4 offset-1 offset-lg-0 offset-md-0   col-md-6 col-sm-12 padClass rightHeroVid">
-              <p className="text-white">PHYGITAL TOY</p>
-              <video autoPlay loop src={video5} className="rightVideo"></video>
+            <div className="col-lg-4     col-md-6 col-sm-12 padClass rightHeroVid">
+              <div className="card">
+                <p className="text-white">PHYGITAL TOY</p>
+                <video
+                  autoPlay
+                  loop
+                  src={video5}
+                  className="rightVideo"
+                ></video>
+              </div>
             </div>
-            <div className="col-lg-4 offset-1 offset-lg-0 offset-md-0   col-md-6 col-sm-12 padClass rightHeroVid">
-              <p className="text-white">PHYGITAL TOY</p>
-              <video autoPlay loop src={video5} className="rightVideo"></video>
+            <div className="col-lg-4     col-md-6 col-sm-12 padClass rightHeroVid">
+              <div className="card">
+                <p className="text-white">PHYGITAL TOY</p>
+                <video
+                  autoPlay
+                  loop
+                  src={video5}
+                  className="rightVideo"
+                ></video>
+              </div>
             </div>
           </div>
         </div>
