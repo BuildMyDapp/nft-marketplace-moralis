2c2e3be47c07561a293a7fa2087384e54f7ce132
reactDev777 <reactdevs777>
2021-10-07 16:47:28 +0500

    Website Complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from "./component/navbar/Navbar";
 import Topbar from "./component/topbar/Topbar";
 import HomeScreen from "./screens/Home/HomeScreen";
 import ListNft from "./screens/ListNft/ListNft";
+import FrontMoney from "./screens/Frontmoney/FrontMoney";
+import Balance from "./screens/Balance/Balance";
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -14,6 +17,8 @@ function App() {
       <div className="container">
         <Route path="/" exact component={HomeScreen} />
         <Route path="/list-nft" component={ListNft} />
+        <Route path="/front-money" component={FrontMoney} />
+        <Route path="/balance" component={Balance} />
       </div>
     </Router>
   );
--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -4,51 +4,67 @@ import { Input } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
 
 const Navbar = () => {
+  const { Search } = Input;
+  const onSearch = (value) => console.log(value);
+
   return (
     <div>
-      <nav className="mx-3 mt-1 navbar navbar-expand-lg    ">
+      {/* <nav className="mx-3 mt-1 navbar navbar-expand-lg    ">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
             Rarible
           </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            {/* <span className="navbar-toggler-icon"></span> */}
-            <i class="navbar-toggler-icon fas fa-bars"></i>
-          </button>
+          <div className="d-flex ">
+            <div className="searchIcon ">
+              <SearchOutlined />
+            </div>
+            <div>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <i
+                  className="  fas fa-bars"
+                  style={{ color: "black !important" }}
+                ></i>
+              </button>
+            </div>{" "}
+          </div>
+
           <div className="col-md-4   col-lg-6 col-sm-12 ms-lg-3 ">
-            <Input
-              placeholder="Collection ,Item or user "
-              className="productSearch"
+            <Search
+              placeholder="Collection ,Item,User"
+              className="searchBtn"
+              onSearch={onSearch}
+              enterButton
+              allowClear
               prefix={<SearchOutlined className="site-form-item-icon" />}
             />
           </div>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ms-2 ms-auto   me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav ms-2  ms-5   me-auto mb-2 mb-lg-0">
               <li className="nav-item ">
-                <a className="nav-link active" aria-current="page" href="#">
+                <a className="nav-link active" aria-current="page" href="/">
                   Marketplace
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <a className="nav-link navL" href="/list-nft">
                   List Nft
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <a className="nav-link navL" href="/balance">
                   Balance Page
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <a className="nav-link navL" href="/front-money">
                   front-money Nft
                 </a>
               </li>
@@ -63,6 +79,76 @@ const Navbar = () => {
                 </a>
               </li>
             </ul>
+          </div>
+        </div>
+      </nav> */}
+
+      <nav className="navbar navbar-expand-lg  ">
+        <div className="container-fluid">
+          <a className=" ms-lg-4 navbar-brand" href="#">
+            Rarible
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i
+              className="  fas fa-bars"
+              style={{ color: "black !important" }}
+            ></i>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 ms-sm-auto mb-lg-0 ms-auto">
+              <li className="nav-item ">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Marketplace
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link navL" href="/list-nft">
+                  List Nft
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link navL" href="/balance">
+                  Balance Page
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link navL" href="/front-money">
+                  front-money Nft
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Wallet
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Connect
+                </a>
+              </li>
+            </ul>
+
+            <form class="d-flex">
+              <input
+                className="form-control  search-data "
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button
+                className="btn btn-outline-success"
+                className="fas fa-search"
+                type="submit"
+              ></button>
+            </form>
           </div>
         </div>
       </nav>
